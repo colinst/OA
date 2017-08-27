1fbb6a87ddf6aa26050f474eac17e739012ffc3f
39805 <398057151@qq.com>
2017-08-28 01:00:16 +0800

    fff

---
+++ web/assets/js/model.js
@@ -0,0 +1,14 @@
+function show() {
+    /*$('#show').css('display','block');*/
+    var a3 = document.getElementById("custom");
+    a3.style.display = "inherit";
+    var a1 = document.getElementById("model");
+    a1.style.display = "none";
+}
+function hide() {
+    var a1 = document.getElementById("custom");
+    a1.style.display = "none";
+    var a3 = document.getElementById("model");
+    a3.style.display = "inherit";
+
+}
